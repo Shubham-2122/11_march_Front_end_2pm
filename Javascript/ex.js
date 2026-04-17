5b051335dba709a2be092ccd3d3dde9d782d8d61
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-04-17 16:51:55 +0530

    js

---
+++ Javascript/ex.js
@@ -0,0 +1,10 @@
+// console.log("Hello External Javascript")
+
+// console.log(document.getElementById("data"))
+// document.getElementById("data").innerHTML = "Hello shubham jadav"
+
+// alert("pls field data")
+// confirm("are your sure?")
+// prompt("5 + 5 = ? ")
+// print()
+
