a57faeb45ac24f8058ce29520306473d2519e041
Shubham-2122 <shubhamjadav.tops@gmail.com>
2026-05-21 12:11:54 +0530

    react

--- react/demo/src/App.js
+++ react/demo/src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
+import Hello from "./JSX/Hello";
+// import ClassCompo from "./Component/ClassCompo";
+// import FunCompo from "./Component/FunCompo";
 
-function App(){
-    return(
-        <h1>Hello this App Js file</h1>
+function App() {
+    return (
+        <div>
+            <h1>Hello this App Js file</h1>
+            
+            {/* compeoent */}
+            {/* <ClassCompo />
+            <ClassCompo />
+            <FunCompo /> */}
+
+            {/* JSX Compoenent */}
+            <Hello />
+
+        </div>
     )
 }
 
