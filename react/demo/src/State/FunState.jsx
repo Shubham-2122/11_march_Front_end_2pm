613638a23d128c31b6840342e45c2558c3672ba2
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2026-05-25 12:07:34 +0530

    react

--- react/demo/src/State/FunState.jsx
+++ react/demo/src/State/FunState.jsx
@@ -7,13 +7,19 @@
 // hooks : Define before Return 
 
 import React, { useState } from 'react'
+import ImageData from './ImageData'
 
 function FunState() {
 
     //    define,change function = hookename(value)
     const [name,setname] = useState("abhinav")
     const [count,setcount] = useState(1)
+    const [Isimage,setisimage] = useState(true)
     console.log(name)
+
+    const increment2=()=>{
+      setcount(count + 2)
+    }
 
   return (
     <div>
@@ -23,6 +29,21 @@ function FunState() {
 
       <h2>Count : {count}</h2>
       <button onClick={()=>setcount(count + 1)}>Increment</button>
+      <button onClick={()=>setcount(count - 1)}>Decrement</button>
+      <button onClick={increment2}>Increment by 2</button>
+
+
+      <button onClick={()=>setcount(0)}>Reset</button>
+      <br /><br /> <br />
+      <hr />
+      <button onClick={()=>setisimage(false)}>Hide</button>
+       <button onClick={()=>setisimage(true)}>Show</button>
+
+        <button onClick={()=>setisimage(!Isimage)}>Toggle</button>
+
+      {
+        Isimage ? <ImageData /> : false
+      }
     </div>
   )
 }
--- react/demo/src/State/MainState.jsx
+++ react/demo/src/State/MainState.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import FunObj from './FunObj'
 // import ClassState from './ClassState'
-import FunState from './FunState'
+// import FunState from './FunState'
 
 function MainState() {
   return (
     <div>
       <h1>Hello this State Component</h1>
       {/* <ClassState />  */}
-      <FunState />
+      {/* <FunState /> */}
+      <FunObj />
     </div>
   )
 }
